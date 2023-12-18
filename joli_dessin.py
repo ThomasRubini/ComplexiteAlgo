c88290cee2557ad3162996b833af27a6a4ec6e32
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-12-18 16:33:30 +0100

    Improve script to draw stuff

--- joli_dessin.py
+++ joli_dessin.py
@@ -1,12 +1,25 @@
+#!/usr/bin/env python3
+
+# Bold lines represent algorithms, other lines are for reference
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("output.csv")
-x = df[df.columns[0]]
+data_x = df[df.columns[0]]
 for col in df.columns[1:]:
-    print()
-    print(df[col])
-    plt.plot(x, df[col], label=df[col].name)
+    plt.plot(data_x, df[col], label=df[col].name, linewidth=3)
+
+x = np.arange(0.01, 50, 0.01)
+FACTOR = 10000
+
+plt.plot(x*FACTOR, np.log(x), label="O(log(n))")
+plt.plot(x*FACTOR, x*np.log(x), label="O(nlog(n))")
+plt.plot(x*FACTOR, x, label="O(n)")
+plt.plot(x*FACTOR, x**2, label="O(n²)")
+
+plt.xlim(0, 50000)
+plt.ylim(0, 5)
 
 plt.title('Sorting algorithm time complexity')
 plt.xlabel('Entry size')
